b6459dfb8fb763209fd512f625be4de6289e9bbc
reyshazni <reyshazni@gmail.com>
2023-06-12 10:22:38 +0700

    add details

---
+++ places_data.py
@@ -0,0 +1,68 @@
+places_data = {
+    "ampera": {
+        "nama": "Jembatan Ampera",
+        "summary": "Jembatan Ampera is a famous bridge that crosses the Musi River in Palembang, South Sumatra, Indonesia...",
+        "rating_tourism": 4.8,
+        "important_facts": [
+            "When Jembatan Ampera was completed in 1965, it became the longest bridge in Southeast Asia, spanning 1,177 meters.",
+            "Jembatan Ampera was initially equipped with elevators in its towers...",
+            "Every year, Jembatan Ampera becomes the venue for the Ampera Bridge Festival..."
+        ],
+        "sejarah": "The construction of Jembatan Ampera began in 1962 under the administration of President Soekarno..."
+    },
+    "gadang": {
+        "nama": "Jam Gadang",
+        "summary": "Jam Gadang is an iconic clock tower located in Bukittinggi, West Sumatra, Indonesia...",
+        "rating_tourism": 4.6,
+        "important_facts": [
+            "One interesting fact about Jam Gadang is that the clock tower was built without the use of nails...",
+            "Jam Gadang has survived and weathered several earthquakes that have struck Bukittinggi over the years...",
+            "During the Dutch colonial period, there was an underground room beneath Jam Gadang that was used as a prison..."
+        ],
+        "sejarah": "The construction of Jam Gadang began in 1926 and was completed in 1928..."
+    },
+    "gwk": {
+        "nama": "Garuda Wisnu Kencana",
+        "summary": "Garuda Wisnu Kencana (GWK) is a monument located in Bukit Ungasan, Bali, Indonesia...",
+        "rating_tourism": 4.5,
+        "important_facts": [
+            "The Garuda Wisnu Kencana statue is one of the largest statues in Indonesia...",
+            "The creation of the Garuda Wisnu Kencana statue involved a complex and time-consuming process...",
+            "Although the main statue has been completed, the Garuda Wisnu Kencana complex is still under development..."
+        ],
+        "sejarah": "The construction of Garuda Wisnu Kencana began in 1997 and is still in development to this day..."
+    },
+    "kotatua": {
+        "nama": "Museum Fatahillah",
+        "summary": "Museum Fatahillah, also known as the Jakarta History Museum, is located in Kota Tua Jakarta, Indonesia...",
+        "rating_tourism": 4.7,
+        "important_facts": [
+            "The museum was originally named Jakarta History Museum when it first opened in 1977...",
+            "Museum Fatahillah has an interesting collection of weapons, including traditional weapons and colonial-era weapons...",
+            "Due to its beautiful architecture and historical atmosphere, Museum Fatahillah is often used as a filming location..."
+        ],
+        "sejarah": "This building has a long history. During the Dutch colonial period, it was used as a courthouse and colonial administration center..."
+    },
+    "monas": {
+        "nama": "Monumen Nasional",
+        "summary": "Monumen Nasional or commonly known as Monas or Tugu Monas is a monument commemorating the resistance and struggle of the Indonesian people...",
+        "rating_tourism": 4.6,
+        "important_facts": [
+            "The gold on the top of Monas was donated by the Indonesian people...",
+            "Before being known as Monas, the building was referred to as Lapangan Gambir, Lapangan Ikada, Lapangan Merdeka...",
+            "The initial design was initially rejected by Soekarno..."
+        ],
+        "sejarah": "The construction of Monas began in 1961 under the administration of President Soekarno..."
+    },
+    "ulundanu": {
+        "nama": "Pura Ulun Danu Bratan",
+        "summary": "Pura Ulun Danu, also known as Pura Ulun Danu Bratan, is a temple located on the shores of Lake Bratan in Bali, Indonesia...",
+        "rating_tourism": 4.7,
+        "important_facts": [
+            "Pura Ulun Danu is one of the few temples in Bali dedicated to worshiping Dewi Danu...",
+            "Pura Ulun Danu gained further popularity after its depiction was used on the 50,000 rupiah banknote...",
+            "In 2012, Pura Ulun Danu was recognized as a UNESCO World Heritage site..."
+        ],
+        "sejarah": "Pura Ulun Danu was founded in the 17th century by King Mengwi, I Gusti Agung Putu..."
+    }
+}
